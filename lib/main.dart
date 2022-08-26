ce9be02ba191cf49785a474913e6da1dbeeb067d
Saud BinGhushayan <Saudstg9@gmail.com>
2022-08-27 00:13:07 +0300

    v7

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(Safra());
 }
 
 final navigatorKey = GlobalKey<NavigatorState>();
