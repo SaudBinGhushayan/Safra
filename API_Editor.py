20840e5bf3be13178f66838cd9c7aabc817ac4a7
abdullah <72196684+RecursiveM@users.noreply.github.com>
2022-09-29 17:21:29 +0300

    Add files via upload
    Version 18 (2)

--- API_Editor.py
+++ API_Editor.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
 
+# In[1]:
 
 
 import requests
@@ -14,7 +17,7 @@ from iso639 import languages
 from textblob import TextBlob
 
 
-# In[3]:
+# In[2]:
 
 
 key = 'fsq3bR9nCSrR/WbzD82rlvh990Q70wuc8BuuRs0Ypm6fx+w='
@@ -23,42 +26,42 @@ key = 'fsq3bR9nCSrR/WbzD82rlvh990Q70wuc8BuuRs0Ypm6fx+w='
 
 def get_latlong(b):
 
-
+    
     try:
-
+    
         city = b
 
         geolocator = Nominatim(user_agent = 'Safra')
-
+    
         loc = geolocator.geocode(city)
-
+         
         # by default
     except: return 'No results found' , f'{b}'
-
+    
     return loc.latitude , loc.longitude
 
 
-# In[4]:
+# In[3]:
 
 
 # this is a test
 lat , long = get_latlong('jeddah')
 
 
-# In[5]:
+# In[4]:
 
 
 lat , long
 
 
-# In[6]:
+# In[5]:
 
 
 def translate(array):
-
+        
     # this list contains new translated descriptions
     tlds = []
-
+    
     for desc in array:
         try:
             # Specifying the language for
@@ -82,29 +85,29 @@ def translate(array):
             else:
                 tlds.append(desc)
         except: tlds.append(desc)
-
+        
     return tlds
+
+
+# In[6]:
+
+
+def extract_categories(array):
+    templist = [array[i][0]['name'] for i in range(len(array))]
+    
+    return templist
+    
 
 
 # In[7]:
 
 
-def extract_categories(array):
-    templist = [array[i][0]['name'] for i in range(len(array))]
-
-    return templist
-
-
-
-# In[8]:
-
-
 def add_photos(array):
-
+    
     return
 
 
-# In[9]:
+# In[8]:
 
 
 def retrieve_places(a , c):
@@ -113,8 +116,8 @@ def retrieve_places(a , c):
     a : condition --- >  example : coffee , art gallery , etc ...
     c : city name
     """
-
-
+    
+    
     lat , long = get_latlong(c)
     if type(lat) != str:
 
@@ -146,29 +149,29 @@ def retrieve_places(a , c):
                                            , 'description' , 'categories']),1,inplace=True)
 
         except: df = df
-
-
-
+            
+        
+        
         """
         in this phase we add empty columns if columns are not available already
-
+        
         ============== remember to change range when changing number of retrieved rows ==============
         """
         if 'price' not in df.columns:
             df.insert(len(df.columns) , 'price' , ['Not Available' for i in range(10)] )
-
+        
         if 'description' not in df.columns:
             df.insert(len(df.columns) , 'description' , ['Not Available' for i in range(10)])
-
+        
         if 'rating' not in df.columns:
             df.insert(len(df.columns) , 'rating' , ['Not Available' for i in range(10)])
 
-
+        
 #         # renaming columns
         if 'location.country' in df.columns and 'location.region' in df.columns:
             df.rename(columns = {'location.country':'country' , 'location.region':'region'}, inplace = True)
-
-
+    
+        
 
 
         # filling nan values
@@ -193,29 +196,29 @@ def retrieve_places(a , c):
             Not Available ---> keep it as it is
 
             english description ---> keep it as it's
-            """
-
+            """ 
+            
             tdl = translate(array)
 
-            # insert it into last
+            # insert it into last 
             df.insert(df.columns.get_loc('description')+1  , 'translated_description' , tdl)
 
-
-
+        
+        
         if 'region' in df.columns:
             array_r = df['region'].to_list()
-
+            
             trl = translate(array_r)
-
+            
             df.insert(df.columns.get_loc('region')+1 , 'translated_region' , trl)
 
         if 'categories' in df.columns:
             templist = df['categories'].to_list()
             templist = extract_categories(templist)
-
+            
             df.drop(['categories'] , inplace = True , axis = 1)
             df.insert(len(df) , 'categories' , templist)
-
+            
         try:
             # changing datatypes
             df = df.astype({'price': str , 'rating': str})
@@ -225,8 +228,8 @@ def retrieve_places(a , c):
         data = df.to_json(orient = 'records')
         return df , data
     else:
-        return lat , long
-
+        return lat , long 
+    
 
 
 # In[ ]:
@@ -235,7 +238,7 @@ def retrieve_places(a , c):
 
 
 
-# In[10]:
+# In[9]:
 
 
 '''
@@ -246,17 +249,11 @@ test field
 '''
 
 
-# In[11]:
+# In[10]:
 
 
 df, data_json = retrieve_places('coffee' , 'riyadh')
 df
-
-
-# In[12]:
-
-
-df['categories'][0][0]
 
 
 # In[13]:
@@ -270,7 +267,7 @@ app = Flask(__name__)
 def index():
     userInputb = str(request.args['query'])
     userInputa = str(request.args['query'])
-    df, data_json = retrieve_places('' , 'riyadh')
+    df, data_json = retrieve_places(userInputa , userInputb)
 
     return data_json
 
@@ -280,3 +277,7 @@ if __name__ == "__main__":
 
 
 # In[ ]:
+
+
+
+
